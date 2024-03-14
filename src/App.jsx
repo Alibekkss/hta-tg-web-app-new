41d961678f17b38df887d48dde7ff90555fef999
Alibekkss <tolegen2102@gmail.com>
2024-03-14 12:51:11 +0500

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+// eslint-disable-next-line no-unused-vars
+const tg  = window.Telegram.WebApp
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-       
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-     
     </>
   )
 }
