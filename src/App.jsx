e05c8dfc1c62e9095c3452f868cea1315498a78e
Alibekkss <tolegen2102@gmail.com>
2024-03-14 13:16:43 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 // eslint-disable-next-line no-unused-vars
 const tg  = window.Telegram.WebApp
 
 function App() {
+  useEffect(()=>{
+    tg.ready();
+  }, [])
 
+  const onCLose = () => {
+    tg.close();
+  }
   return (
     <>
       <div>
-        
+          <button onClick={onClose}>Закрыть</button>
       </div>
     </>
   )
